4868b8e32023d55000dd39609c4d4e956841f4cf
UserGit-Dev <welcometorapture@hotmail.it>
2026-02-26 17:23:51 +0100

    3

--- GestioneStudenti/Program.cs
+++ GestioneStudenti/Program.cs
@@ -8,16 +8,17 @@ class GestionStudenti
 
     public static void VisualizzaRegistro()
     {
+        Console.WriteLine(new string('=', 30));
         for (int i = 0; i < voti.GetLength(0); i++)
         {
-            
-            Console.WriteLine("=== " + studenti[i] + " ===");
+            Console.WriteLine($"Scheda Ricerca: {studenti[i]}");
+            Console.WriteLine(new string('-', 30));
             for (int j = 0; j < voti.GetLength(1); j++)
             {
                 // Ciclo tutte le colonne di "voti" per stampare la materia attuale (usando j) e i voti
                 Console.WriteLine($"{materie[j], 2}: {voti[i,j], 2}");
             }
-            Console.WriteLine("---------------\n");
+            Console.WriteLine(new string('=', 30));
         }
 
         Console.WriteLine("\nPremere un tasto per continuare...");
@@ -25,15 +26,15 @@ class GestionStudenti
         Console.Clear();
     }
 
-    public static void Ricerca(string studente, int voto)
+    public static void Ricerca(string studente, int sogliaVoto)
     {
-        if (string.IsNullOrEmpty(studente) || voto is < 1 or > 10)
+        if (string.IsNullOrEmpty(studente) || sogliaVoto is < 1 or > 10)
         {
-            Console.WriteLine("Errore, rispettare i requisiti richiesti. (Nome e voto da 1 a 10)");
+            Console.WriteLine("Errore: Inserire un nome valido e un voto tra 1 e 10.");
         }
 
-        studente = studente.Substring(0, 1).ToUpper() + studente.Substring(1).ToLower();
-        int idxStudente = studenti.IndexOf(studente);
+        studente = char.ToUpper(studente[0]) + studente.Substring(1).ToLower();
+        int idxStudente = Array.IndexOf(studenti, studente);
 
         if (idxStudente is -1) 
         {
@@ -47,12 +48,15 @@ class GestionStudenti
 
         for (int j = 0; j < voti.GetLength(1); j++)
         {
-            if (voti[idxStudente, j] >= voto) { materieVoti.Add(materie[j], voti[idxStudente, j]); }
+            if (voti[idxStudente, j] >= sogliaVoto) { materieVoti.Add(materie[j], voti[idxStudente, j]); }
         }
 
         if (materieVoti.Count > 0) {
-            Console.WriteLine("===== " + studente + " =====");
-            Console.WriteLine($"Voto maggiore/uguale a: {voto}\n");
+            Console.WriteLine(new string('=', 30));
+            Console.WriteLine($"Scheda Ricerca: {studente}");
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine($"Filtro: Voto ≥ {sogliaVoto}");
+            Console.WriteLine(new string('-', 30));
             foreach (var materiaVoto in materieVoti) { 
                 Console.WriteLine($"{materiaVoto.Key} => {materiaVoto.Value}");
             }
@@ -60,6 +64,7 @@ class GestionStudenti
             Console.WriteLine("Non è stato trovato nessuno studente che rispetti i filtri forniti.");     
         }
 
+        Console.WriteLine(new string('=', 30));
         Console.WriteLine("\nPremere un tasto per continuare...");
         Console.ReadKey(true);
         Console.Clear();
