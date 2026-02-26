d24ab0a346481ec7d16e8411ee57e158b435323a
Nambon03 <deciccodev@gmail.com>
2026-02-26 17:47:42 +0100

    Progetto Merge
    Update:
    + eseguito merge correttamente sulla branch

--- GestioneStudenti/obj/Debug/net10.0/GestioneStudenti.AssemblyInfo.cs
+++ GestioneStudenti/obj/Debug/net10.0/GestioneStudenti.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestioneStudenti")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff47cda8b27a3d3fb144b4e3a132d79c124ef054")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88bb751913b8284b0770f73ee98d48e356cb4f40")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestioneStudenti")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestioneStudenti")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
